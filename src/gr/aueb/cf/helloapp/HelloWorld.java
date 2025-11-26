5723c50a8d60c6c0034c674ca4858f33c7074318
AthanasiaPet <athanasia.petsa@yahoo.com>
2025-11-26 22:12:03 +0200

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project is created to show
+ * how to make a git project.
+ * And then how to stage and commit
+ * changes in local repo.
+ * And then how to push ti GitHub.
+ */
+public class HelloWorld {
+}
